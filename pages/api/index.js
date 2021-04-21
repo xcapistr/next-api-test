62775cc678a409cb5de8f9de385dcec121dc2d46
capism01 <marek.capistrak01@dixonscarphone.com>
2021-04-21 12:19:27 +0200

    init next with mongo

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,14 @@
+import { connectToDatabase } from '../../util/mongodb'
+
 export default async (req, res) => {
-    if (req.method === "GET") {
-        res.status(200).send("hey");
-    }
-};
+  const { db } = await connectToDatabase()
+  if (req.method === 'GET') {
+    const bla = await db
+      .collection('next-api-test')
+      .find({})
+      .sort({ metacritic: -1 })
+      .limit(20)
+      .toArray()
+    res.json(bla)
+  }
+}
