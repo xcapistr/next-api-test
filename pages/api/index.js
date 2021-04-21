3c01cd72cb834449c92e0d15a4e99dd8d5881c87
capism01 <marek.capistrak01@dixonscarphone.com>
2021-04-21 12:45:53 +0200

    add and read data

--- pages/api/index.js
+++ pages/api/index.js
@@ -3,12 +3,18 @@ import { connectToDatabase } from '../../util/mongodb'
 export default async (req, res) => {
   const { db } = await connectToDatabase()
   if (req.method === 'GET') {
-    const bla = await db
+    const response = await db
       .collection('next-api-test')
       .find({})
       .sort({ metacritic: -1 })
       .limit(20)
       .toArray()
-    res.json(bla)
+    res.json(response)
+  }
+
+  if (req.method === 'POST') {
+    const data = req.body
+    const response = await db.collection('next-api-test').insertOne(data)
+    res.json(response)
   }
 }
